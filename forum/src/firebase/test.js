6afb8ec62a1acf0126da4ac5a20432d5056fd46b
Svetoslav <sberberski@gmail.com>
2024-08-01 13:27:08 +0300

    multi Update with Create

--- forum/src/firebase/test.js
+++ forum/src/firebase/test.js
@@ -1,24 +1,57 @@
 import {batchUpdates} from "./batchUpdates.js";
+import { ref, push } from 'firebase/database';
+import { get, set, query, equalTo, orderByChild, update } from 'firebase/database';
+import { db } from "./config.js";
+// const categoryOneId = 1;
+// const categoryTwoId = 2;
 
-const categoryOneId = 1;
-const categoryTwoId = 2;
+// const objectOne = `Role/${1}`;
+// const objectTwo = `Role/${2}`;
+// const objectThree = `Role/${3}`;
 
-const objectOne = `category/${categoryOneId}`;
-const objectTwo = `category/${categoryTwoId}`;
+// const updates = {
+//     // Creating new data
+//     [objectOne]: {name: 'User', subcategories: ['']},
+//     [objectTwo]: {name: 'Mod', subcategories: ['']},
+//     [objectThree]: {name: 'Admin', subcategories: ['']},
+// };
 
-const updates = {
-    // Creating new data
-    [objectOne]: {name: 'Information', subcategories: []},
-    [objectTwo]: {name: 'PC Gaming', subcategories: []},
-};
+// try {
+//     const message = await batchUpdates(updates);
+//     console.log(message)
+// } catch (e) {
+//     console.log(e)
+// }
 
-try {
-    const message = await batchUpdates(updates);
-    console.log(message)
-} catch (e) {
-    console.log(e)
+const createPostAndUpdate = async (info) => {
+    let categoryId = 2;
+    const result = await push(ref(db, `post`), info);
+    const id = result.key;
+
+    
+    const categories = [2, 3, 4]; 
+
+    const updatePromises = categories.map(categoryId => {
+        return update(ref(db), {
+            [`category/${categoryId}/posts/${id}`]: true,
+        });
+    });
+
+    await Promise.all(updatePromises);
 }
 
+
+const info={
+    'Title': 'Test',
+    'Content': "Test",
+    'CreatedOn': new Date().toDateString(),
+    'User': {
+        'Test': 'Pesho'
+    }
+}
+
+
+createPostAndUpdate(info);
 // subcategoru/category
 // posts/subcategory/
 // replies/post/
