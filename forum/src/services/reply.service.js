10b731be8147157037ede652aba583d67386b51c
Svetoslav <sberberski@gmail.com>
2024-08-02 19:02:47 +0300

    Reply.Service

--- forum/src/services/reply.service.js
+++ forum/src/services/reply.service.js
@@ -1,9 +1,25 @@
+import { createElement, createPath, removeElement, updateElement } from "../firebase/firebase-funcs";
+
 export const createReply = async (info, postID, subcategoriesId) => {
 
     const path= createPath('reply', postID)
     const id= await createElement(info, path);
-    const pathForUpdate=  createPath('Posts', subcategoriesId, postID, 'replyID', id)
-    // post/ID/POSTID/replyID
-    await updateElement(true, pathForUpdate);
-    const pathForUserUpdate= createPath('User', info.CreatedBy.username)
+    const pathForUpdate=  createPath('Posts', subcategoriesId, postID, 'updatedOn')
+    // post/ID/POSTID/updatedOn
+    await updateElement(info.createdOn, pathForUpdate);
+    const pathForUserUpdate= createPath('user', info.createdBy.username)
+    await createElement (id, pathForUserUpdate);
+}
+
+export const updateReply= async (info, postID, replyID) => {
+
+    const path = createPath('reply', postID, replyID);
+    await updateElement (info, path);
+
+}
+
+export const deleteReply = async(postID, replyID) =>{
+
+    const path = createPath('reply', postID, replyID);
+    await removeElement(path);
 }
