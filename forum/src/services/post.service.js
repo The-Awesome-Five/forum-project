b6110418314c032b47e23a86114e6e8a4def5e75
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-08-02 19:26:24 +0300

    post.service-update

--- forum/src/services/post.service.js
+++ forum/src/services/post.service.js
@@ -1,36 +1,24 @@
-export const batchPosts = async (postData) => {
-    try {
-      const postRequests = postData.map(post =>
-        fetch('/home/topics', { 
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            SubcategoryID: post.SubcategoryID,
-            Title: post.Title,
-            Content: post.Content,
-            CreatedBy: {
-              ID: post.CreatedBy.ID
-            },
-            CreatedOn: post.CreatedOn,
-            NumberOfReplies: post.NumberOfReplies,
-            Likes: post.Likes,
-            ReportsID: post.ReportsID,
-            Deleted: post.Deleted,
-            RepliesId: post.RepliesId,
-            LatestReplyDate: post.LatestReplyDate
-          })
-        })
-      );
-  
-      const responses = await Promise.all(postRequests);
-      const results = await Promise.all(responses.map(response => response.json()));
-      console.log('Response:', results);
-      return results;
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  };
-  
+import { createElement, createPath, removeElement, updateElement } from "../firebase/firebase-funcs";
+
+export const createPost = async (postInfo, subcategoriesId) => {
+    const path = createPath('posts');
+    const postId = await createElement(postInfo, path);
+
+    const pathForUpdate = createPath('Posts', subcategoriesId, postId, 'updatedOn');
+    await updateElement(postInfo.createdOn, pathForUpdate);
+
+    const pathForUserUpdate = createPath('user', postInfo.createdBy.username);
+    await createElement(postId, pathForUserUpdate);
+
+    return postId;
+};
+
+export const updatePost = async (postInfo, postId) => {
+    const path = createPath('posts', postId);
+    await updateElement(postInfo, path);
+};
+
+export const deletePost = async (postId) => {
+    const path = createPath('posts', postId);
+    await removeElement(path);
+};
