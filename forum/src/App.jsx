0bf54c6516d81f4be2270ca45f3d4367b241bdbc
donetian-petkov <donetianpetkov@gmail.com>
2024-08-07 13:44:02 +0300

    Category Component + View Initial Commit

--- forum/src/App.jsx
+++ forum/src/App.jsx
@@ -1,35 +1,39 @@
 import {useEffect, useState} from 'react'
 import './App.css'
 import {createElement, getElement} from "./firebase/firebase-funcs.js";
-import { Routes, Route } from 'react-router-dom';
-import HeaderBar from "./components/headerBar/HeaderBar.jsx";
+import {Routes, Route} from 'react-router-dom';
+import HeaderBar from "./components/commonComponents/HeaderBar/HeaderBar.jsx";
+import {Category} from "./components/commonComponents/CategoryComponent/Category.jsx";
+import {Home} from "./views/HomeView/Home.jsx";
 
 function App() {
-  const [elements, setElements] = useState({});
+    const [elements, setElements] = useState({});
 
     useEffect(() => {
         getElement('Roles')
             .then(elems => setElements(elems));
     }, []);
 
-  return (
+    return (
 
-    <>
-    <HeaderBar />
-    <Routes>
-     {/* <Route path="/" element={<Home />} />
+        <>
+            <HeaderBar/>
+            <Home/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+              {/*
       <Route path="/latest" element={<Latest />} />
       <Route path="/top" element={<Top />} />
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />} />*/}
-    </Routes>
-  </>
+            </Routes>
+        </>
 
-    // <>
-    //   <h1>Testing</h1>
-    //     <button onClick={() => createElement('user', 'Roles')}>Create</button>
-    // </>
-  )
+        // <>
+        //   <h1>Testing</h1>
+        //     <button onClick={() => createElement('user', 'Roles')}>Create</button>
+        // </>
+    )
 }
 
 export default App
---
+++ forum/src/views/HomeView/Home.jsx
@@ -0,0 +1,12 @@
+import {Category} from "../../components/commonComponents/CategoryComponent/Category.jsx";
+import {CategoryView} from "../CategoryView/CategoryView.jsx";
+
+export const Home = () => {
+
+    return (
+        <div className='home'>
+           <CategoryView/>
+        </div>
+    )
+
+}
