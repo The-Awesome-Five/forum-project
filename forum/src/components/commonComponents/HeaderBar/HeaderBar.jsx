91d249a72e478bf2e8a8739853920617168698df
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-08-08 02:43:32 +0300

    dropdown-menu

--- forum/src/components/commonComponents/HeaderBar/HeaderBar.jsx
+++ forum/src/components/commonComponents/HeaderBar/HeaderBar.jsx
@@ -1,30 +1,52 @@
-import React from "react";
-import './HeaderBar.css'
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import './HeaderBar.css';
+import { Link, useNavigate } from "react-router-dom";
 
 const HeaderBar = () => {
+    const navigate = useNavigate();
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const handleLogoClick = () => {
+        navigate('/');
+    };
+
+    const handleProfileClick = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
     return (
-        <div className="headerbar">
+        <div className="header-wrapper">
+            
+            <div className="headerbar">
+                <div className="logo" onClick={handleLogoClick}>
+                    <img src="/img/GameHub-logo.png" alt="logo"/>
+                </div>
 
-            <div className="logo">
-                <img src="/img/GameHub-logo.png" alt="logo"/>
+                <div className="nav-links">
+                    <Link to="/latest">LATEST</Link>
+                    <Link to="/top">TOP</Link>
+                    <Link to="/about">ABOUT</Link>
+                </div>
+
+                <div className="dropdown">
+                    <div className="profile-icon" onClick={handleProfileClick} tabIndex="0">
+                        <img src="/img/profile-icon.png" alt="profile icon" />
+                    </div>
+                    <div className={`dropdown-menu ${dropdownOpen ? 'show' : ''}`}>
+                        <div className="reg-btn">
+                            <Link to="/register">REGISTER ▶️</Link>
+                        </div>
+                        <div className="log-btn">
+                            <Link to="/login">LOGIN       ▶️</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="nav-links">
-                 <Link to="/latest">LASTEST</Link>
-                 <Link to="/top">TOP</Link>
-                 <Link to="/about">ABOUT</Link>
-                 <Link to='/register'> REGISTER</Link>
+            <div className="banner">
+                <img src="img/banner.png" alt="Banner" />
             </div>
-
-            <div className="profile-icon">
-                <Link to="/profile">
-                    <img src="/img/profile-icon.png" alt="" />
-                </Link>
-            </div>
-
         </div>
-    )
+    );
 }
 
 export default HeaderBar;
