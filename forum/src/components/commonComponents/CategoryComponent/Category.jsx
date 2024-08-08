7fa7a4f4bf19303cf35f7bb4a6f175098f42bc50
donetian-petkov <donetianpetkov@gmail.com>
2024-08-08 13:30:35 +0300

    Subcategory Grid Layout

--- forum/src/components/commonComponents/CategoryComponent/Category.jsx
+++ forum/src/components/commonComponents/CategoryComponent/Category.jsx
@@ -13,9 +13,11 @@ export const Category = () => {
                 <p>For all of your PC Gaming stuff</p>
             </div>
             <div className='category-subcategory'>
-                <Link to='/category/pc/hardware'> <img src="/img/subcategory-logo.png" alt="logo"/> PC Hardware
-                </Link>
-                <Link to='/category/pc/games'>  <img src="/img/subcategory-logo.png" alt="logo"/> PC Games </Link>
+                <Link to='/category/pc/hardware'> <img src="/img/subcategory-logo.png" alt="logo"/> PC Hardware</Link>
+                <Link to='/category/pc/games'> <img src="/img/subcategory-logo.png" alt="logo"/> PC Games </Link>
+                <Link to='/category/pc/news'> <img src="/img/subcategory-logo.png" alt="logo"/> PC News </Link>
+                <Link to='/category/pc/hardware'> <img src="/img/subcategory-logo.png" alt="logo"/> PC Hardware</Link>
+                <Link to='/category/pc/games'> <img src="/img/subcategory-logo.png" alt="logo"/> PC Games </Link>
                 <Link to='/category/pc/news'> <img src="/img/subcategory-logo.png" alt="logo"/> PC News </Link>
             </div>
         </div>
