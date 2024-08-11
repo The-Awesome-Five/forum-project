802113fa67f0134dc586f6458542a8eb64a6f983
donetian-petkov <donetian+petkov@gmail.com>
2024-08-11 18:59:04 +0300

    Admin Category View Layout Ready

--- forum/src/components/adminComponents/AdminCategoryComponent/AdminCategoryItem.jsx
+++ forum/src/components/adminComponents/AdminCategoryComponent/AdminCategoryItem.jsx
@@ -1,4 +1,5 @@
 import './AdminCategoryItem.css'
+import {Link} from "react-router-dom";
 
 export const AdminCategoryItem = ({category}) => {
 
@@ -8,11 +9,16 @@ export const AdminCategoryItem = ({category}) => {
             <li>{category.name}</li>
             <li>{category.description}</li>
             <ul>
-                {Object.values(category.subcategory_ids).map(subcategory => <li key={subcategory.id}>{subcategory.name}</li>)}
+                {Object.values(category.subcategory_ids).map(subcategory => <li key={subcategory.id}>
+                    {category.isHeader
+                        ? subcategory.name
+                        :<Link to={`/category/${category.id}/${subcategory.id}`}>{subcategory.name}</Link>
+                    }</li>)}
             </ul>
             {category.isHeader ? "Buttons" :
                 <div className="admin-category-item-buttons">
                     <button>Edit</button>
+                    <button>Hide</button>
                     <button>Delete</button>
                 </div>
             }
