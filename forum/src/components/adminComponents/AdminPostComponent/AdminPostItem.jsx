bfd4f1a0853d9d42811e861591d116f8b4ebead3
donetian-petkov <donetian+petkov@gmail.com>
2024-08-12 00:44:05 +0300

    Admin Post Menu Initial Commit

---
+++ forum/src/components/adminComponents/AdminPostComponent/AdminPostItem.jsx
@@ -0,0 +1,34 @@
+import {useEffect, useState} from "react";
+import {getSubcategoriesByPostId} from "../../../services/post.service.js";
+import './AdminPostItem.css'
+import {getSubcategoryNameBySubcategoryId} from "../../../services/subcategory.service.js";
+
+export const AdminPostItem = ({post}) => {
+
+    const [subcategory, setSubcategory] = useState('')
+
+    useEffect(() => {
+        if(!post.isHeader) {
+            getSubcategoryNameBySubcategoryId(post.subcategoryId)
+                .then(data => setSubcategory(data.name))
+                .catch(e => alert(e));
+        }
+    }, []);
+
+    return (
+        <ul className={post.isHeader ? "admin-post-item-header" : "admin-post-item"}>
+            <li>{post.Title}</li>
+            <li>{post.Content}</li>
+            <li>{post.isHeader ? "Created By" : post.createdBy.username}</li>
+            <li>{post.CreatedOn}</li>
+            <li>{post.isHeader ? 'Subcategory' : subcategory}</li>
+            {post.isHeader ? "Buttons" :
+                <div className="admin-post-item-buttons">
+                    <button>Hide</button>
+                    <button>Lock</button>
+                    <button>Delete</button>
+                </div>
+            }
+        </ul>
+    )
+}
