880d501ae70b798c24a7b91cf48133865eb70ec7
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-08-12 15:29:36 +0300

    Admin Subcategory Hide Ready

--- forum/src/components/adminComponents/AdminSubcategory/AdminSubcategoryComponent/AdminSubcategoryItem.jsx
+++ forum/src/components/adminComponents/AdminSubcategory/AdminSubcategoryComponent/AdminSubcategoryItem.jsx
@@ -1,19 +1,55 @@
 import './AdminSubcategoryItem.css'
 import {useEffect, useState} from "react";
-import {getCategoryNameBySubcategoryId} from "../../../../services/category.service.js";
+import {
+    getCategoryIdBySubcategoryId,
+    getCategoryNameBySubcategoryId,
+    hideCategory,
+    showCategory
+} from "../../../../services/category.service.js";
 import {Link} from "react-router-dom";
+import {hideSubcategory, showSubcategory} from "../../../../services/subcategory.service.js";
 
 export const AdminSubcategoryItem = ({subcategory}) => {
 
-    const [categoryName, setCategoryName] = useState('');
+    const [category, setCategory] = useState({
+        name:'',
+        id: ''
+    });
+    const [isHidden, setHidden] = useState(subcategory.isHidden);
 
     useEffect(() => {
         if(!subcategory.isHeader && subcategory.id) {
             getCategoryNameBySubcategoryId(subcategory.id)
-                .then(data => setCategoryName(data.name))
+                .then(data => setCategory(sub => {
+                    return {
+                        ...sub,
+                        name: data
+                    }}))
                 .catch(e => alert('Failed to get category name', e));
             }
+        getCategoryIdBySubcategoryId(subcategory.id)
+            .then(data => setCategory(sub => {
+                return {
+                    ...sub,
+                    id: data
+            }}))
+            .catch(e => alert('Failed to get category id', e));
+
     }, [])
+
+    const hideHandler = async () => {
+        try {
+            if (!isHidden) {
+                await hideSubcategory(category.id, subcategory.id);
+                setHidden(true);
+            } else {
+                await showSubcategory(category.id, subcategory.id);
+                setHidden(false);
+            }
+        } catch (e) {
+            alert(e)
+        }
+    }
 
 
     return (
@@ -21,11 +57,11 @@ export const AdminSubcategoryItem = ({subcategory}) => {
             <li className="admin-subcategory-item-image">{subcategory.isHeader ? "Subcategory Image" :
                 <img src={subcategory.imgUrl} alt="subcategory-imgUrl"/>}</li>
             <li>{subcategory.name}</li>
-            <li>{subcategory.isHeader ? "Parent Category" : categoryName}</li>
+            <li>{subcategory.isHeader ? "Parent Category" : category.name}</li>
             {subcategory.isHeader ? "Buttons" :
                 <div className="admin-subcategory-item-buttons">
                     <button><Link to={"/edit-subcategory"} state={{ subcategoryToBeEdited: subcategory }}>Edit</Link></button>
-                    <button>Hide</button>
+                    <button onClick={hideHandler}>{isHidden ? 'Show' : 'Hide'}</button>
                     <button>Lock</button>
                 </div>
             }
--- forum/src/services/category.service.js
+++ forum/src/services/category.service.js
@@ -77,7 +77,7 @@ export const getCategoryNameBySubcategoryId = async (subcategory_id) => {
     try {
         const categories = await getElement('Category');
         const category = Object.values(categories).find(category => category.subcategory_ids[subcategory_id]);
-        return category;
+        return category.name;
     } catch (e) {
         console.error('Failed to get category id by subcategory id', e);
         return e.message;
