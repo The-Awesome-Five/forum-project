419fe6c9de25ca4155b16ec1314cf32e2b4f5e77
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-08-13 23:40:00 +0300

    create-reply-from-style

--- forum/src/components/commonComponents/CreateReplyForm/CreateReplyForm.jsx
+++ forum/src/components/commonComponents/CreateReplyForm/CreateReplyForm.jsx
@@ -2,66 +2,66 @@ import { useContext, useState } from "react";
 import { AppContext } from "../../../state/app.context";
 import { useNavigate, useParams } from "react-router-dom";
 import { createReply } from "../../../services/reply.service";
-export const CreateReplyForm = () =>  {
-    const { userData } = useContext(AppContext);
-    const { subcategoryId } = useParams();
-    const {postId} = useParams();
-    const {categoryId}= useParams();
-const [reply, setReply] = useState({
+
+export const CreateReplyForm = () => {
+  const { userData } = useContext(AppContext);
+  const { subcategoryId, postId, categoryId } = useParams();
+
+  const [reply, setReply] = useState({
     createdBy: {
-        username: '',
-        ID: ''
+      username: '',
+      ID: ''
     },
-    Content:'',
+    Content: '',
     CreatedOn: '',
     isHidden: false,
     isLocked: false,
-
   });
 
   const navigate = useNavigate();
+
   const updatePost = (prop) => (e) => {
-
     setReply((prevPost) => ({
-
       ...prevPost,
       [prop]: e.target.value,
     }));
   };
 
-  const create= async() => {
+  const create = async () => {
     const { Content } = reply;
-    if(!Content){
-        return alert('Please fill all of the needed fields');
+    if (!Content) {
+      return alert('Please fill all of the needed fields');
     }
-    if(Content.length < 10){
-        return alert('Content needs to be above 10 characters');
+    if (Content.length < 10) {
+      return alert('Content needs to be above 10 characters');
     }
 
     try {
-        reply.CreatedOn=new Date().toString();
-        reply.createdBy.ID= userData.uid
-        reply.createdBy.username= userData.username
-        const replyID= await createReply(reply,postId, subcategoryId);
+      reply.CreatedOn = new Date().toString();
+      reply.createdBy.ID = userData.uid;
+      reply.createdBy.username = userData.username;
+      const replyID = await createReply(reply, postId, subcategoryId);
 
-        navigate(0);
+      navigate(0);
+    } catch (e) {
+      // Handle the error
     }
-    catch(e){
-    }
-  }
+  };
 
-return (
-  <div>
-        <br/>
-        <textarea
-        placeholder="Content"
+  return (
+    <div className="create-reply-container">
+      <textarea
+        className="reply-textarea"
+        placeholder="Write your reply..."
         value={reply.Content}
         onChange={updatePost('Content')}
         rows="5"
       />
+      <button className="submit-reply-button" onClick={create}>
+        Post Reply
+      </button>
+    </div>
+  );
+};
 
-         <br/>
-         <button onClick={create}>Post</button>
-  </div>
-)
-}
+export default CreateReplyForm;
