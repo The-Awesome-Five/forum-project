d4fcc71299216ea53792a025eeba14f1c7cfe9e0
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-08-13 23:50:31 +0300

    create-from-style

--- forum/src/components/commonComponents/CreateForm/CreateForm.jsx
+++ forum/src/components/commonComponents/CreateForm/CreateForm.jsx
@@ -1,83 +1,85 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../../state/app.context";
 import { useNavigate, useParams } from "react-router-dom";
 import { createPost } from "../../../services/post.service";
-import { getUserByID } from "../../../services/user.service";
-import { updateUsersPostWithSubCat } from "../../../services/user.service";
+import './CreateFrom.css'
 
-export const CreatePost = () =>  {
+export const CreatePost = () => {
     const { userData } = useContext(AppContext);
     const { subcategoryId } = useParams();
 
-const [post, setPost] = useState({
-    createdBy: {
-        username: '',
-        ID: ''
-    },
-    Title:'',
-    Content:'',
-    CreatedOn: '',
-    isHidden: false,
-    isLocked: false,
-    likedBy: {
-      temp: null,
-    },
+    const [post, setPost] = useState({
+        createdBy: {
+            username: '',
+            ID: ''
+        },
+        Title: '',
+        Content: '',
+        CreatedOn: '',
+        isHidden: false,
+        isLocked: false,
+        likedBy: {
+            temp: null,
+        },
+    });
 
-  });
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
-  const updatePost = (prop) => (e) => {
+    const updatePost = (prop) => (e) => {
+        setPost((prevPost) => ({
+            ...prevPost,
+            [prop]: e.target.value,
+        }));
+    };
 
-      setPost((prevPost) => ({
+    const create = async () => {
+        const { Title, Content } = post;
+        if (!Title || !Content) {
+            return alert('Please fill all of the needed fields');
+        }
+        if (Title.length < 16 || 64 < Title.length) {
+            return alert('Title should be between 16 and 64 symbols');
+        }
+        if (Content.length < 32 || 8192 < Content.length) {
+            return alert('Content needs to be between 32 and 8192 symbols');
+        }
 
-      ...prevPost,
-      [prop]: e.target.value,
-    }));
-  };
+        try {
+            post.CreatedOn = new Date().toString();
+            post.createdBy.ID = userData.uid;
+            post.createdBy.username = userData.username;
+            const postID = await createPost(post, subcategoryId);
 
-  const create= async() => {
-    const { Title, Content } = post;
-    if(!Title  || !Content){
-        return alert('Please fill all of the needed fields');
-    }
-    if(Title.length < 16 || 64 < Title.length){
-        return alert('Title should be between 16 and 64 symbols');
-    }
-    if(Content.length < 32 || 8192 < Content.length){
-        return alert('Content needs to be between 32 and 8192 symbols');
-    }
+            navigate('/');
+        } catch (e) {
+            
+        }
+    };
 
-    try {
-        post.CreatedOn=new Date().toString();
-        post.createdBy.ID= userData.uid
-        post.createdBy.username= userData.username
-        const postID= await createPost(post, subcategoryId);
+    return (
+        <div className="create-post-container">
+            <h2>Create A Post</h2>
+            <input
+                type="text"
+                className="post-title-input"
+                placeholder="Title"
+                value={post.Title}
+                onChange={updatePost('Title')}
+            />
+            <br />
+            <textarea
+                className="post-content-textarea"
+                placeholder="Content"
+                value={post.Content}
+                onChange={updatePost('Content')}
+                rows="10"
+            />
+            <br />
+            <button className="submit-post-button" onClick={create}>
+                Post
+            </button>
+        </div>
+    );
+};
 
-        navigate('/');
-    }
-    catch(e){
-    }
-  }
-
-return (
-  <div>
-        <h2>Create A Post</h2>
-        <input
-          type="text"
-          placeholder="Title"
-          value={post.Title}
-          onChange={updatePost('Title')}
-        />
-        <br/>
-        <textarea
-        placeholder="Content"
-        value={post.Content}
-        onChange={updatePost('Content')}
-        rows="5"
-      />
-
-         <br/>
-         <button onClick={create}>Post</button>
-  </div>
-)
-}
+export default CreatePost;
